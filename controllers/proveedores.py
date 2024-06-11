cbd7b6118b7184df25b37a05037aa567606db373
DAV1104 <90273298+DAV1104@users.noreply.github.com>
2024-06-11 05:13:49 -0500

    Delete controllers directory

--- controllers/proveedores.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, jsonify, render_template, request, redirect
-from config.db import app, db, ma
-from models.Proveedor import Proveedor, ProveedoresSchema
-
-ruta_proveedores = Blueprint("route_proveedores", __name__)
-
-proveedor_schema= ProveedoresSchema()
-proveedores_schema= ProveedoresSchema(many=True)
